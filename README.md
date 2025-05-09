491356d8340332e72bd1714d1dc9824eb9016988
orhanunalbjss <orhan.unal@bjss.com>
2025-05-09 14:16:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Activity Manager
+
+Activity Manager is an application that helps you to organise your activities. You can add, update, or delete activities. You can also add a random activity, if you need a little inspiration.
---
+++ src/main/java/com/ounal/activity_manager/ActivityManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ounal.activity_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityManagerApplication.class, args);
+	}
+
+}
