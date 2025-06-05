7ac2efa51400d7a12ccbdb1b12919353b64954cb
Orhan Unal <orhan.unal@bjss.com>
2025-06-05 10:37:35 +0100

    Add bean to allow requests to come from any origin, allowing frontend to talk to backend

--- src/main/java/com/ounal/activity_manager/AppConfig.java
+++ src/main/java/com/ounal/activity_manager/AppConfig.java
@@ -2,7 +2,10 @@ package com.ounal.activity_manager;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +13,17 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")  // Applies CORS to all endpoints
+                        .allowedOrigins("*") // Allows requests from any domain
+                        .allowedMethods("*"); // Defines allowed HTTP methods
+            }
+        };
     }
 }
