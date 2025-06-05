efb60b981cb78ee8e870531b31343e8f733b34a4
Orhan Unal <orhan.unal@bjss.com>
2025-06-05 18:23:49 +0100

    Add API integration

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
@@ -20,24 +20,25 @@ import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
+import {CircularProgress} from "@mui/material";
 
-function AddNewActivityButton({handleOpen}) {
+function AddNewActivityButton({loading, handleOpenAddDialog}) {
   return (
-    <Button variant="contained" onClick={handleOpen}>
+    <Button variant="contained" disabled={loading} onClick={handleOpenAddDialog}>
       Add New Activity
     </Button>
   );
 }
 
-function AddRandomActivityButton() {
+function AddRandomActivityButton({loading, handleAddRandomActivity}) {
   return (
-    <Button variant="contained">
+    <Button variant="contained" disabled={loading} onClick={handleAddRandomActivity}>
       Add Random Activity
     </Button>
   );
 }
 
-function AddActivityButtons({handleOpen}) {
+function AddActivityButtons({loading, handleOpenAddDialog, handleAddRandomActivity}) {
   return (
     <Box
       sx={{
@@ -46,17 +47,17 @@ function AddActivityButtons({handleOpen}) {
         gap: 1,
       }}
     >
-      <AddNewActivityButton handleOpen={handleOpen}/>
-      <AddRandomActivityButton/>
+      <AddNewActivityButton loading={loading} handleOpenAddDialog={handleOpenAddDialog}/>
+      <AddRandomActivityButton loading={loading} handleAddRandomActivity={handleAddRandomActivity}/>
     </Box>
   );
 }
 
-function EditActivityButton({activity, handleOpenEdit}) {
+function EditActivityButton({activity, onEdit}) {
   return (
     <Tooltip title="Edit">
       <IconButton
-        onClick={() => handleOpenEdit(activity.name, activity.type, activity.participants)}
+        onClick={() => onEdit(activity)}
         color="primary"
         aria-label="edit">
         <EditIcon/>
@@ -65,57 +66,47 @@ function EditActivityButton({activity, handleOpenEdit}) {
   );
 }
 
-function DeleteActivityButton() {
+function DeleteActivityButton({id, onDelete}) {
   return (
     <Tooltip title="Delete">
-      <IconButton color="primary" aria-label="delete">
+      <IconButton
+        onClick={() => onDelete(id)}
+        color="primary"
+        aria-label="delete">
         <DeleteIcon/>
       </IconButton>
     </Tooltip>
   );
 }
 
-function ActivityActions({activity, handleOpenEdit}) {
+function ActivityActions({activity, onEdit, onDelete}) {
   return (
     <Box align="center">
-      <EditActivityButton activity={activity} handleOpenEdit={handleOpenEdit}/>
-      <DeleteActivityButton/>
+      <EditActivityButton activity={activity} onEdit={onEdit}/>
+      <DeleteActivityButton id={activity.id} onDelete={onDelete}/>
     </Box>
   );
 }
 
-function ActivityRow({activity, handleOpenEdit}) {
+function ActivityRow({activity, onEdit, onDelete}) {
   return (
     <TableRow
-      key={activity.name}
       sx={{'&:last-child td, &:last-child th': {border: 0}}}
     >
-      <TableCell component="th" scope="row">
-        {activity.name}
-      </TableCell>
+      <TableCell>{activity.name}</TableCell>
       <TableCell>{activity.type}</TableCell>
       <TableCell align="right">{activity.participants}</TableCell>
-      <TableCell align="right"><ActivityActions activity={activity} handleOpenEdit={handleOpenEdit}/></TableCell>
+      <TableCell align="right">
+        <ActivityActions activity={activity} onEdit={onEdit} onDelete={onDelete}/>
+      </TableCell>
     </TableRow>
   );
 }
 
-function ActivityTable({activities, handleOpenEdit}) {
-  const activityRows = [];
-
-  activities.forEach((activity) => {
-    activityRows.push(
-      <ActivityRow
-        activity={activity}
-        key={activity.name}
-        handleOpenEdit={handleOpenEdit}
-      />
-    );
-  });
-
+function ActivityTable({activities, onEdit, onDelete}) {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{minWidth: 650}} aria-label="simple table">
+      <Table sx={{minWidth: 650}}>
         <TableHead>
           <TableRow>
             <TableCell>Name</TableCell>
@@ -125,42 +116,71 @@ function ActivityTable({activities, handleOpenEdit}) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {activityRows}
+          {activities.map((activity) =>
+            <ActivityRow
+              key={activity.id}
+              activity={activity}
+              onEdit={onEdit}
+              onDelete={onDelete}/>
+          )}
         </TableBody>
       </Table>
     </TableContainer>
   );
 }
 
-function AddEditActivityModal({open, handleClose, handleSave, handleChange, formData}) {
+function ActivityDialog({open, onClose, onSave, editingActivity}) {
+  const [activity, setActivity] = useState({name: "", type: "", participants: ""});
+
+  useEffect(() => {
+    if (editingActivity) {
+      setActivity(editingActivity);
+    } else {
+      setActivity({name: "", type: "", participants: ""});
+    }
+  }, [editingActivity]);
+
+  const handleSave = () => {
+    onSave(activity);
+    handleClose();
+  };
+
+  const handleClose = () => {
+    setActivity({name: "", type: "", participants: ""});
+    onClose();
+  };
+
   return (
     <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Add/Edit Activity</DialogTitle>
+      <DialogTitle>{editingActivity ? "Edit Activity" : "Create Activity"}</DialogTitle>
       <DialogContent>
         <TextField
+          fullWidth
           margin="normal"
           label="Name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          fullWidth
+          value={activity.name}
+          onChange={(e) => setActivity(
+            {...activity, name: e.target.value}
+          )}
         />
         <TextField
+          fullWidth
           margin="normal"
           label="Type"
-          name="type"
-          value={formData.type}
-          onChange={handleChange}
-          fullWidth
+          value={activity.type}
+          onChange={(e) => setActivity(
+            {...activity, type: e.target.value}
+          )}
         />
         <TextField
+          fullWidth
           margin="normal"
           label="Participants"
-          name="participants"
           type="number"
-          value={formData.participants}
-          onChange={handleChange}
-          fullWidth
+          value={activity.participants}
+          onChange={(e) => setActivity(
+            {...activity, participants: e.target.value}
+          )}
         />
       </DialogContent>
       <DialogActions>
@@ -175,35 +195,81 @@ function AddEditActivityModal({open, handleClose, handleSave, handleChange, form
   );
 }
 
-function ActivityManagerApp({activities}) {
-  const [open, setOpen] = useState(false);
-  const [formData, setFormData] = useState({
-    name: "",
-    type: "",
-    participants: "",
-  });
+function ActivityManagerApp() {
+  const [activities, setActivities] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [editingActivity, setEditingActivity] = useState(null);
 
-  const handleOpen = () => setOpen(true);
-  const handleOpenEdit = (name, type, participants) => {
-    setFormData({name: name, type: type, participants: participants,});
-    setOpen(true);
-  }
-  const handleClose = () => {
-    setFormData({name: "", type: "", participants: "",});
-    setOpen(false);
-  }
-
-  const handleChange = (e) => {
-    const {name, value} = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+  const fetchData = () => {
+    setLoading(true);
+    fetch("http://localhost:8080/activities")
+      .then((res) => res.json())
+      .then((data) => {
+        setActivities(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
   };
 
-  const handleSave = () => {
-    console.log("Form Data:", formData);
-    handleClose();
+  useEffect(() => {
+    fetchData()
+  }, []);
+
+  const handleAddRandomActivity = () => {
+    fetch("http://localhost:8080/activities/random", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+    })
+      .then((res) => res.json())
+      .then(() => {
+        fetchData();
+      });
+  };
+
+  const handleOpenAddDialog = () => {
+    setEditingActivity(null);
+    setOpenDialog(true);
+  }
+
+  const handleOpenEditDialog = (activity) => {
+    setEditingActivity({
+      id: activity.id,
+      name: activity.name ?? "",
+      type: activity.type ?? "",
+      participants: activity.participants ?? "",
+    });
+    setOpenDialog(true);
+  }
+
+  const handleSaveActivity = (activity) => {
+    const method = editingActivity ? "PUT" : "POST";
+    const url = editingActivity
+      ? `http://localhost:8080/activities/${editingActivity.id}`
+      : "http://localhost:8080/activities";
+    const jsonWithoutId = JSON.stringify(activity, (key, value) => {
+      return key === "id" ? undefined : value;
+    });
+
+    fetch(url, {
+      method,
+      body: jsonWithoutId,
+      headers: {"Content-Type": "application/json"},
+    })
+      .then((res) => res.json())
+      .then(() => {
+        fetchData();
+        setOpenDialog(false);
+      });
+  };
+
+  const handleDeleteActivity = (id) => {
+    fetch(`http://localhost:8080/activities/${id}`, {
+      method: "DELETE",
+    }).then(() => fetchData());
   };
 
   return (
@@ -223,27 +289,31 @@ function ActivityManagerApp({activities}) {
         You can add, update, or delete activities.
         You can also add a random activity, if you need a little inspiration.
       </Typography>
-      <AddActivityButtons handleOpen={handleOpen}/>
-      <ActivityTable activities={activities}
-                     handleOpenEdit={handleOpenEdit}/>
-      <AddEditActivityModal open={open}
-                            handleClose={handleClose}
-                            handleSave={handleSave}
-                            handleChange={handleChange}
-                            formData={formData}
+      <AddActivityButtons
+        loading={loading}
+        handleOpenAddDialog={handleOpenAddDialog}
+        handleAddRandomActivity={handleAddRandomActivity}/>
+      {loading ? (
+        <Box sx={{display: "flex", justifyContent: "center", mt: 3}}>
+          <CircularProgress/>
+        </Box>
+      ) : (
+        <ActivityTable
+          activities={activities}
+          onEdit={handleOpenEditDialog}
+          onDelete={handleDeleteActivity}
+        />
+      )}
+      <ActivityDialog
+        open={openDialog}
+        onClose={() => setOpenDialog(false)}
+        onSave={handleSaveActivity}
+        editingActivity={editingActivity}
       />
     </Box>
   );
 }
 
-const ACTIVITIES = [
-  {
-    name: "My awesome activity!",
-    type: "recreational",
-    participants: 1
-  },
-]
-
 export default function Home() {
-  return <ActivityManagerApp activities={ACTIVITIES}/>
+  return <ActivityManagerApp/>
 }
